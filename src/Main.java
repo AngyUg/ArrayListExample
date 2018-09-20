01f89833eaefef6545b8aa264c5efc7abb1c2004
AngyUg <proofmyself@gmail.com>
2018-09-20 19:12:49 +0300

    ArrayListExampleCRUD

--- src/Main.java
+++ src/Main.java
@@ -4,36 +4,20 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String menU;
-        boolean theEnd = false;
-
-        List<String> list = new ArrayList();
-        list.add("15");
-        Scan scan = new Scan();
         Menu menu = new Menu();
 
-        menu.mainMenu();
+        List<String> list = new ArrayList<>();
+       // list.add("15");
 
-        scan.readConsoleValue().equalsIgnoreCase("c");
-        {
-            System.out.println("enter value to add in our collection: ");
-            list.add(scan.readConsoleValue());
-            System.out.println(list + "- new value in collection and hashCode is: " + list.hashCode());
-        }
+//        menu.mainMenu();
+//        menu.addCollection((ArrayList<String>) list);
+//        System.out.println( menu.readCollection((ArrayList<String>) list));
+//        menu.updateCollection((ArrayList<String>) list);
+//        menu.deleteCollection((ArrayList<String>) list);
 
-        scan.readConsoleValue().equalsIgnoreCase("r");
-        {
-            System.out.println("Read our collection: ");
-            System.out.println(list + "- new value in collection and hashCode is: " + list.hashCode());
-        }
-
-        scan.readConsoleValue().equalsIgnoreCase("d");
-        {
-            list.clear();
-            System.out.println(list + " - elements of collection deleted and hashCode is: " + list.hashCode());
-
-
-
+        while (!menu.theEnd){
+            menu.mainMenu();
+            list=menu.routeCollection((ArrayList<String>) list);
         }
 
 
--- src/Menu.java
+++ src/Menu.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
+    boolean theEnd = false;
+
     public void mainMenu() {
         Scan scan = new Scan();
         System.out.println("Menu:\n" +
@@ -13,12 +17,62 @@ public class Menu {
 
     }
 
+    public ArrayList<String> routeCollection(ArrayList<String> list) {
+        String command = readConsoleValue();
+
+        if (command.equalsIgnoreCase("c")) {
+            list = addCollection((ArrayList<String>)list);
+        } else if (command.equalsIgnoreCase("r")) {
+            list = readCollection(list);
+        } else if (command.equalsIgnoreCase("u")) {
+            list = updateCollection(list);
+        } else if (command.equalsIgnoreCase("d")) {
+            list = deleteCollection(list);
+        } else if (command.equalsIgnoreCase("exit")) {
+            System.out.println("If yes write 'exit' again");
+            theEnd = checkExit();
+        } else {
+            System.out.println("упс, шось не те:)");
+        }
+
+        return list;
+    }
+
+
     public boolean checkExit() {
         if (readConsoleValue().equalsIgnoreCase("exit")) {
             return true;
         } else {
             return false;
         }
+    }
+
+    public ArrayList<String> addCollection(ArrayList<String> list) {
+ //       readConsoleValue().equalsIgnoreCase("c");
+        System.out.println("enter value to add in our collection: ");
+        list.add(readConsoleValue());
+        System.out.println(list + "- our collection and hashCode is: " + list.hashCode());
+        return list;
+    }
+
+    public ArrayList<String> readCollection(ArrayList<String> list) {
+       // readConsoleValue().equalsIgnoreCase("r");
+            System.out.println("Read our collection: "+list);
+        return list;
+    }
+
+    public ArrayList<String> updateCollection(ArrayList<String> list) {
+        System.out.println("First, enter index not longer then: "+(list.size()-1)+", then value to change element in collection");
+        list.set(Integer.parseInt(readConsoleValue()), readConsoleValue());
+        System.out.println(list + "- collection and hashCode is: " + list.hashCode());
+        return list;
+    }
+
+    public ArrayList<String> deleteCollection(ArrayList<String> list) {
+        System.out.println("Enter index to delete element in collection not longer then: "+(list.size()-1));
+        list.remove(Integer.parseInt(readConsoleValue()));
+        System.out.println(list + "- collection after deleted element");
+        return list;
     }
 
     protected String readConsoleValue() {
